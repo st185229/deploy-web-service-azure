13f4b2fca78d6b0cfce83faf8958be40c10d1618
Suresh Thomas <st185229@ncr.com>
2021-03-29 01:43:02 +0100

    add NSG , networks

--- C1 - Azure Infrastructure Operations/solution/nsg.tf
+++ C1 - Azure Infrastructure Operations/solution/nsg.tf
@@ -23,16 +23,16 @@ resource "azurerm_network_security_rule" "allow_access_from_intenet_80" {
 }
 
 resource "azurerm_network_security_rule" "allow_access_from_vm_on_subnet" {
-  name                        = "access_from_same_subnet"
-  priority                    = 1002
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefixes       = var.network_address_spaces["vnet_address_spaces"]
-  destination_address_prefixes  = var.network_address_spaces["vnet_address_spaces"]
-  resource_group_name         = azurerm_resource_group.rg_web.name
-  network_security_group_name = azurerm_network_security_group.allow_access.name
+  name                         = "access_from_same_subnet"
+  priority                     = 1002
+  direction                    = "Inbound"
+  access                       = "Allow"
+  protocol                     = "Tcp"
+  source_port_range            = "*"
+  destination_port_range       = "*"
+  source_address_prefixes      = var.network_address_spaces["vnet_address_spaces"]
+  destination_address_prefixes = var.network_address_spaces["vnet_address_spaces"]
+  resource_group_name          = azurerm_resource_group.rg_web.name
+  network_security_group_name  = azurerm_network_security_group.allow_access.name
 }
 
